7e07c1f5de149132df583121a1b5029c3d99d00c
Ronald Rink <ronald.rink@d-fens.net>
2015-03-31 17:26:11 +0200

    2015-03-31; rrink; ADD: jython jar to directly support Python scripts

--- src/test/java/biz/dfch/j/graylog2/plugin/output/ExecScriptTest.java
+++ src/test/java/biz/dfch/j/graylog2/plugin/output/ExecScriptTest.java
@@ -173,14 +173,46 @@ public class ExecScriptTest
         }
         
         message = new Message("my Message", "mySource", DateTime.now());
+<<<<<<< HEAD
         assertEquals(true, true);
+=======
+        message.addField("myField", "myValue");
+>>>>>>> 2015-03-31; rrink; ADD: jython jar to directly support Python scripts
 
         Configuration configuration = new Configuration(map);
         Stream stream = new TestStream();
         
         dfchBizExecScript execScript = new dfchBizExecScript(stream, configuration);
+<<<<<<< HEAD
         message.addField("myField", "myValue");
         
     }
 
 }
+=======
+
+        // if we end up here instantiation was successful
+        assertEquals(true, true);
+    }
+
+}
+
+/**
+ *
+ *
+ * Copyright 2015 Ronald Rink, d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+>>>>>>> 2015-03-31; rrink; ADD: jython jar to directly support Python scripts
